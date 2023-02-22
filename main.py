7a69fdd0c572c760e797031125fa7644a4cb0036
HIWHY <hiwhy@proton.me>
2023-02-22 20:04:46 +0530

    Add Which payment method choice function

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ database = DataBaseConnect()
 vid_cap = cv2.VideoCapture(0)  # Access the webcam
 
 # # Sample Demo user
-chi_details = {"first_name": "Che",'last_name':'Das', "image": "my_smart_image2.jpg", "money": 500}
-zen_details = {"first_name": "Zen", 'last_name':'Das','image': "zen.jpg", 'money': 200}
+chi_details = {"first_name": "Che", 'last_name': 'Das', "image": "my_smart_image2.jpg", "money": 500}
+zen_details = {"first_name": "Zen", 'last_name': 'Das', 'image': "zen.jpg", 'money': 200}
 
 # This is demo code for add new user
 # database.add_new_user(chi_details['first_name'],chi_details['last_name'],chi_details['image'],chi_details['money'])
@@ -17,10 +17,31 @@ zen_details = {"first_name": "Zen", 'last_name':'Das','image': "zen.jpg", 'money
 img = cv2.imread(chi_details['image'])
 device_user_detail = database.get_self_user_data(img)
 
-
 face_detect_run = False
 
-if device_user_detail is not None:
+
+def payment_method_choice():
+    bot_talk.say_speach("Which payment method you choose DIRECT or UPI: ")
+    payment_method = bot_talk.input_speach()
+    print(payment_method)
+    if payment_method.find("direct") != -1 or payment_method.find('DIRECT') != -1:
+        return 'direct'
+    elif payment_method.find('upi') != -1 or payment_method.find("UPI") != -1:
+        return 'upi'
+    else:
+        return 'none'
+
+
+while True:
+    payment_ch = payment_method_choice()
+    if payment_ch == 'direct':
+        break
+    elif payment_ch == 'upi':
+        bot_talk.say_speach("Currently Not available This Option")
+    else:
+        bot_talk.say_speach('Please Choice Right Option')
+
+if device_user_detail is not None and payment_ch == 'direct':
     self_user_firstname = device_user_detail[0]
     self_user_lastname = device_user_detail[1]
     self_user_uuid = device_user_detail[2]
@@ -30,7 +51,8 @@ if device_user_detail is not None:
             question = "How much money to Pay: "
             bot_talk.say_speach(question)
             money = bot_talk.input_speach()
-            money = money.replace("Rs", "").replace("$", "").replace("rupees", "").replace('dollar','').replace("dollars",'')
+            money = money.replace("Rs", "").replace("$", "").replace("rupees", "").replace('dollar', '').replace(
+                "dollars", '')
             print(money)
 
             if money.find("home") != -1 or money.find("HOME") != -1:
